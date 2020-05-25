46fb3eb847c4bc3d630c371f947ff0e9dc82aa4d
rhys-hanrahan <rhys.hanrahan@gmail.com>
2020-05-25 19:25:49 +1000

    Fixed Flash message class to not re-use Flash instance if the session does not match the current session instance.

--- src/java/uts/isd/util/Flash.java
+++ src/java/uts/isd/util/Flash.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package uts.isd.util;
+package uts.isd.util;   
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -30,7 +30,8 @@ public class Flash {
     
     public static Flash getInstance(HttpSession session)
     {
-        if (Flash.instance != null)
+        //Check to make sure session is valid to avoid session already invalidated error.
+        if (Flash.instance != null && Flash.instance.sessionMatches(session))
             return Flash.instance;
         
         Flash.instance = new Flash(session);
@@ -50,6 +51,11 @@ public class Flash {
         }
     }
     
+    public boolean sessionMatches(HttpSession session)
+    {
+        return (this.session != null && this.session.equals(session));
+    }
+    
     public void add(MessageType type, String message)
     {
         FlashMessage m = new FlashMessage(type, message);
@@ -66,15 +72,7 @@ public class Flash {
         for (int i = 0; i < this.messages.size(); i++)
         {
             FlashMessage message = this.messages.get(i);
-            
-            if (message.IsDisplayed())
-            {
-                deleteIndexes.add(i);
-                continue;
-            }
-            
             messages += message.outputMessage();
-            message.setDisplayed();
             deleteIndexes.add(i);
         }
         
@@ -94,23 +92,11 @@ public class Flash {
         
         private MessageType type;
         private String value;
-        private boolean isDisplayed;
         
         public FlashMessage(MessageType type, String value)
         {
             this.type = type;
             this.value = value;
-            this.isDisplayed = false;
-        }
-        
-        public boolean IsDisplayed()
-        {
-            return this.isDisplayed;
-        }
-        
-        public void setDisplayed()
-        {
-            this.isDisplayed = true;
         }
         
         public String outputMessage()
@@ -133,7 +119,6 @@ public class Flash {
             out += "  "+ this.value +"\n";
             out += "</div>\n\n";
             
-            this.isDisplayed = true;
             return out;
         }
     }
