9e8724949cc5419ed3ae4ce7f8f213af0dd983c8
rhys-hanrahan <rhys.hanrahan@gmail.com>
2020-05-30 00:26:18 +1000

    Fixed bug with Flash messages where displaying multiple messages caused an index out of bounds error during displayMessages

--- src/java/uts/isd/controller/UsersController.java
+++ src/java/uts/isd/controller/UsersController.java
@@ -133,6 +133,18 @@ public class UsersController extends HttpServlet {
         try {
             HttpSession session = request.getSession();
             
+            Validator validator = new Validator(new ValidationMethod[] {
+                new ValidateRequired("Email", "email"),
+                new ValidateEmail("Email", "email"),
+                //new ValidateTrim("Email", "email"),
+                new ValidateRequired("Password", "password")
+            });
+            
+            if (!validator.validate(request))
+            {
+                response.sendRedirect(request.getHeader("referer"));
+            }
+            
             //Create a connection to the DB for users table
             IUser dbUser = new DBUser();
             User user = dbUser.authenticateUser(request.getParameter("email"), request.getParameter("password"));
